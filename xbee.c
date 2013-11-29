8e3c1dcbd889e2f65c59d6adfed757788c8d9481
Christophe VG <xtof@astroboy.local>
2013-11-29 11:42:24 +0100

    added support for two USARTS to XBEE added support for receiving made sending and receiving more generic

--- xbee.c
+++ xbee.c
@@ -3,35 +3,30 @@
 
 // functions to access an XBee S2/ZB from AVR
 
-#include <string.h>
-
-#include <avr/io.h>
-#include <util/delay.h>
-
-#include "avr.h"
 #include "xbee.h"
 
-void xbee_send_char(char c) {
-  loop_until_bit_is_set(UCSR0A, UDRE0);
-  UDR0 = c;
-}
+// one RX packet handler
+static xbee_rx_handler_t rx_handler;
 
-uint8_t xbee_get_char(void) {
-  while( !(UCSR0A & (1<<RXC0)) );
-  return(UDR0);
-}
+// QnD solution, pre-allocating 256 handlers :-(
+static xbee_at_handler_t at_handlers[0xFF];
+static uint8_t           at_handler_id = 0;
 
-void xbee_wait_for_association(void) {
-  uint8_t value      = 0xFF;
-  uint8_t id         = 0x00;
-  
-  while(value != 0) {
-    _delay_ms(10); /* breath */
-    id = id + 1;
-    xbee_at('A', 'I', id);
-    value = xbee_get_response(0x88, id, 'A', 'I');
-  }
-}
+// global AI response
+static uint8_t ai_response;
+
+static void    xbee_send_byte(uint8_t c);
+static void    xbee_wait_until_tx_complete();
+static uint8_t xbee_receive_byte(void);
+static bool    xbee_data_available(void);
+static void    xbee_receive_rx(uint8_t size);
+static void    xbee_send_at(uint8_t ch1, uint8_t ch2, xbee_at_handler_t *hndlr);
+static void    xbee_receive_at(uint8_t size);
+static void    xbee_check_ai();
+static bool    xbee_ai_success();
+static void    xbee_handle_ai(uint8_t status, uint8_t response);
+
+// public interface
 
 void xbee_sleep(void) {
   // power down XBee by setting its sleep pin high
@@ -44,80 +39,170 @@ void xbee_wakeup(void) {
   xbee_wait_for_association();
 }
 
-void xbee_at(uint8_t ch1, uint8_t ch2, uint8_t id) {
-  xbee_send_char(0x7E);   // start frame
-  
-  xbee_send_char(0x00);   // length MSB
-  xbee_send_char(0x04);   // length LSB
-
-  xbee_send_char(0x08);   // Frame type = AT
-  xbee_send_char(id);     // Frame ID
-  xbee_send_char(ch1);    // AT command char 1
-  xbee_send_char(ch2);    // AT command char 2
-
-  long sum = 0x08 + id + ch1 + ch2;
-  xbee_send_char(0xFF - (sum & 0xFF) ); // checksum
+void xbee_wait_for_association(void) {
+  do { xbee_check_ai(); } while( ! xbee_ai_success() )
 }
 
-uint8_t xbee_get_response(uint8_t type, uint8_t id, uint8_t ch1, uint8_t ch2) {
-  // TODO: make really generic
-  while( xbee_get_char() != 0x7E ) {} // wait for start of frame
-  xbee_get_char(); // skip MSB length, we know length
-  xbee_get_char(); // skip LSB length, we know length
-  if(xbee_get_char() == type) {
-    if( xbee_get_char() == id ) {
-      if( xbee_get_char() == ch1 ) {
-        if( xbee_get_char() == ch2 ) {
-          if( xbee_get_char() == 0x00 ) {      // command statue == OK
-            return xbee_get_char();            // return command data
-          }
-        }
-      }
-    }
+void xbee_send(xbee_tx_t *frame) {
+  xbee_send_byte(XB_FRAME_START);
+
+  // split out size + 14 bytes of protocol overhead into MSByte en LSByte
+  xbee_send_byte((frame->size + 14) >> 8);
+  xbee_send_byte(frame->size + 14);
+
+  xbee_send_byte(XB_TX_PACKET);       // Frame type = transmit
+  long sum = XB_TX_PACKET;
+  
+  xbee_send_byte(frame->id);
+  sum += frame->id;
+
+  // 64-bit address (MSB -> LSB)
+  for(uint8_t i=56;i>=0;i-=8) {
+    xbee_send_byte(frame->address >> i);
+    sum += (frame->address >> i) & 0x00000000000000FF;
   }
-  return 0xFF; // TODO: do this differently ?
-}
-
-void xbee_transmit(uint8_t* ch, uint8_t length) {
-  xbee_send_char(0x7E);   // start frame
-  
-  xbee_send_char(0x00);   // length MSB
-
-  xbee_send_char(length+14);   // length LSB = payload length + 14 "overhead"
-
-  xbee_send_char(0x10);  // Frame type = transmit
-  xbee_send_char(0x00);  // no response (FOR NOW == TODO)
-
-  // 64-bit address
-  // coordinator == 0x0000000000000000
-  xbee_send_char(0x00);    xbee_send_char(0x00);
-  xbee_send_char(0x00);    xbee_send_char(0x00);
-  xbee_send_char(0x00);    xbee_send_char(0x00);
-  xbee_send_char(0x00);    xbee_send_char(0x00);
 
   // 16-bit address
-  xbee_send_char(0xFF);    xbee_send_char(0xFE);
-  
-  xbee_send_char(0x00);  // broadcast radius
-  
-  xbee_send_char(0x00);  // options
-  
-  // data + checksum calc
-  long sum = 0x10 + 0xFF + 0xFE;
-  int8_t i;
-  for(i=0;i<length;i++) {
-    xbee_send_char(ch[i]);
-    sum = sum + ch[i];
-  }
-  
-  xbee_send_char(0xFF - (sum & 0xFF) ); // checksum
+  xbee_send_byte(frame->nw_address >> 8);
+  sum += frame->nw_address >> 8;
+  xbee_send_byte(frame->nw_address     );
+  sum += frame->address & 0x00FF;
 
-  // make sure everything is transmitted
-  // TODO: how to do this CLEANLY?
-  _delay_ms(100);
+  xbee_send_byte(frame->radius);  // broadcast radius
+  sum += frame->radius;
+
+  xbee_send_byte(frame->options);  // options
+  sum += frame->options;
+
+  // data
+  for(int8_t i=0;i<frame->size;i++) {
+    xbee_send_byte(frame->data[i]);
+    sum += frame->data[i];
+  }
+
+  xbee_send_byte(0xFF - (sum & 0xFF) ); // checksum
 }
 
-void xbee_transmit_string(char* string) {
-  // TODO: convert char* to uint8* to remove warnings
-  xbee_transmit(string, strlen(string));
+void xbee_receive(void) {
+  while( xbee_data_available() ) {
+    while( xbee_get_byte() != XB_FRAME_START ) {} // wait for start of frame
+    uint8_t size = (xbee_get_byte() << 8) | (xbee_get_byte() & 0xFF); // get size
+
+    // switch on frame type
+    switch( xbee_get_byte() ) {
+      case XB_RX_PACKET: xbee_receive_rx(size); break;
+      case XB_RX_AT    : xbee_receive_at(size); break;
+      // whoops, unsupported :-)
+    }
+}
+
+void xbee_on_receive(xbee_rx_handler_t handler) {
+  rx_handler = handler;
+}
+
+// internals
+
+static void xbee_send_byte(uint8_t c) {
+  loop_until_bit_is_set(UCSRxA, UDREx); // wait until USART Data Reg Empty
+  UDR0 = c;
+}
+
+static void xbee_wait_until_tx_complete() {
+  loop_until_bit_is_set(UCSRxA, TXCx);  // wait until TX Complete
+}
+
+static uint8_t xbee_receive_byte(void) {
+  while( ! xbee_data_available() );
+  return(UDRx);
+}
+
+static bool xbee_data_available(void) {
+  return UCSRxA & (1<<RXCx);
+}
+
+static void xbee_receive_rx(uint8_t size) {
+  xbee_rx_t frame;
+
+  uint64_t  address;
+  uint16_t  nw_address;
+  uint8_t   options;
+  uint8_t  *data = (uint8_t*)malloc(size-12); // 12 bytes are protocol overhead
+  
+  // 64-bit address (MSB -> LSB)
+  for(uint8_t i=56; i>=0; i-=8) {
+    address |= xbee_get_byte() << i;
+  }
+
+  // 16bit network address
+  nw_address = (xbee_get_byte() << 8) | (xbee_get_byte());
+
+  options = xbee_get_byte();
+  
+  // data
+  for(int8_t i=0; i<size-12; i++) {
+    data[i] = xbee_get_byte();
+  }
+  
+  // create a frame and have it taken care of
+  frame.size       = size;
+  frame.address    = address;
+  frame.nw_address = nw_address;
+  frame.options    = options;
+  frame.data       = data;
+
+  rx_handler(&frame);
+}
+
+static void xbee_send_at(uint8_t ch1, uint8_t ch2, xbee_at_handler_t *handler) {
+  // install response handler, id maps to entry in table
+  at_handlers[at_handler_id] = handler;
+
+  // send frame
+  xbee_send_byte(XB_START_FRAME);
+  
+  xbee_send_byte(0x00);          // MSB
+  xbee_send_byte(0x04);          // LSB (fixed length)
+
+  xbee_send_byte(XB_TX_AT);
+  xbee_send_byte(at_handler_id); // frame ID
+  xbee_send_byte(ch1);           // AT command char 1
+  xbee_send_byte(ch2);           // AT command char 2
+
+  long sum = 0x08 + id + ch1 + ch2;
+  xbee_send_byte(0xFF - (sum & 0xFF) ); // checksum
+  
+  at_handler_id++;
+}
+
+static void xbee_receive_at(uint8_t size) {
+  uint8_t id  = xbee_get_byte();
+  uint8_t ch1 = xbee_get_byte();
+  uint8_t ch2 = xbee_get_byte();
+
+  xbee_at_handler_t handler = at_handlers[id];
+
+  uint8_t status = xbee_get_byte();
+
+  uint8_t data = 0;
+  if(size-5 > 0) {  // command data available ?
+    data = xbee_get_byte();
+  }
+
+  handler(status, data);
+}
+
+static void xbee_check_ai() {
+  ai_response = XB_AT_AI_SCANNING; // seems most logical non-ok default value
+  xbee_send_at('A', 'I', xbee_handle_ai_response);
+  _delay_ms(10);  // TODO: tune this
+}
+
+static bool xbee_ai_success() {
+  return ai_response == XB_AT_AI_SUCCESS;
+}
+  
+static void xbee_handle_ai(uint8_t status, uint8_t response) {
+  if(status == XB_AT_OK) {
+    ai_response = response;
+  }
 }
--- xbee.h
+++ xbee.h
@@ -3,18 +3,101 @@
 
 // functions to access an XBee S2/ZB from AVR
 
+#ifndef __XBEE_H
+#define __XBEE_H
+
+#include <string.h>
+
+#include <avr/io.h>
+#include <util/delay.h>
+
+#include "avr.h"
+
+// magic bytes
+
+// internal
+#define XB_FRAME_START      0x7E
+#define XB_TX_PACKET        0x10
+
+// externally useable
+#define XB_RX_PACKET        0x90
+#define XB_TX_AT            0x08
+#define XB_RX_AT            0x88
+
+#define XB_TX_NO_RESPONSE   0x00      // id to use when no response wanted
+#define XB_BROADCAST        0xFFFE    // nw_address for broadcast
+#define XB_NW_ADDR_UNKNOWN  0xFFFE    //             or unknown
+#define XB_MAX_RADIUS       0x00
+#define XB_OPT_NO_ACK       0x01
+#define XB_OPT_APS_ENC      0x20
+#define XB_OPT_EXT_TIMEOUT  0x40
+
+// AT command status
+#define XB_AT_OK            0x00
+#define XB_AT_ERROR         0x01
+#define XB_AT_INV_CMD       0x02
+#define XB_AT_INV_PARAM     0x03
+#define XB_AT_TX_FAIL       0x04
+
+// AT AI responses
+#define XB_AT_AI_SUCCESS    0x00
+#define XB_AT_AI_SCANNING   0xFF
+
+// XBEE is controled via USART, some AVR devices have multiple USARTs
+// the identifying 0 or 1 is replace by an 'x'
+
+#ifdef XBEE_ON_USART1     // USART1 (e.g. on the ATMEGA1284p)
+#define UCSRxA UCSR1A
+#define UDREx  UDRE1
+#define RXCx   RXC1
+#define UDRx   UDR1
+#define TXCx   TXC1
+#else                     // USART0 (default)
+#define UCSRxA UCSR0A
+#define UDREx  UDRE0
+#define RXCx   RXC0
+#define UDRx   UDR0
+#define TXCx   TXC0
+#endif
+
+// pin mapping
+
 #define XBEE_SLEEP 2
 
-void     xbee_send_char(char c);
-uint8_t  xbee_get_char(void);
+// TX struct
+typedef struct {
+  uint16_t size;
+  uint8_t  id;
+  uint64_t address;
+  uint16_t nw_address;
+  uint8_t  radius
+  uint8_t  options;
+  uint8_t *data;
+} xbee_tx_t;
 
-void     xbee_wait_for_association(void);
+// RX struct
+typedef struct {
+  uint16_t size;
+  uint64_t address;
+  uint16_t nw_address;
+  uint8_t  options;
+  uint8_t *data;
+} xbee_rx_t;
 
-void     xbee_at(uint8_t ch1, uint8_t ch2, uint8_t id);
-uint8_t  xbee_get_response(uint8_t type, uint8_t id, uint8_t ch1, uint8_t ch2);
+// RX handler type
+typedef void (*xbee_rx_handler_t)(xbee_rx_t *frame);
 
-void     xbee_transmit(uint8_t* bytes, uint8_t length);
-void     xbee_transmit_string(char* string);
+// AT response handler type
+typedef void (*xbee_at_handler_t)();
 
-void     xbee_sleep(void);
-void     xbee_wakeup(void);
+// functions
+
+void xbee_sleep(void);
+void xbee_wakeup(void);
+void xbee_wait_for_association(void);
+
+void xbee_send(xbee_tx_t *frame);
+void xbee_receive();
+void xbee_on_receive(xbee_rx_handler_t handler);
+
+#endif
