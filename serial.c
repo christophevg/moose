8d410e839844d99ff32ffdc73e89c1758aa1c217
Christophe VG <xtof@astroboy.local>
2013-11-29 11:41:41 +0100

    added support for two USARTS to serial

--- serial.c
+++ serial.c
@@ -3,10 +3,6 @@
 
 // functions to operate the UART on AVR/ATMega
 
-#include <stdio.h>
-#include <avr/io.h>
-
-#include "avr.h"
 #include "serial.h"
 
 // trick to make printf "print" to serial<->UART
@@ -18,25 +14,32 @@ void serial_init(void) {
   // make RX pin input pin by clearing it
   avr_clear_bit(RX_PORT, RX_PIN);
 
-  // set USART Baud rate
-  UBRR0H = MYUBRR >> 8;
-  UBRR0L = MYUBRR;
-  // enable receiver and transmitter
-  UCSR0B = (1 << RXEN0) | (1 << TXEN0);
-    
+  // Thanks: http://www.appelsiini.net/2011/simple-usart-with-avr-libc
+  UBRRsH = UBRRH_VALUE;
+  UBRRsL = UBRRL_VALUE;
+
+#if USE_2X
+  UCSRsA |= _BV(U2Xs);
+#else
+  UCSRsA &= ~(_BV(U2Xs));
+#endif
+
+  UCSRsC = _BV(UCSZs1) | _BV(UCSZs0); // 8-bit data
+  UCSRsB = _BV(RXENs)  | _BV(TXENs);  // Enable RX and TX
+
   stdout = &mystdout; // required for printf init
 }
 
 static int serial_putchar(char c, FILE *stream) {
   if (c == '\n') serial_putchar('\r', stream); // add a CR before the LF
 
-  loop_until_bit_is_set(UCSR0A, UDRE0);
-  UDR0 = c;
+  loop_until_bit_is_set(UCSRsA, UDREs);
+  UDRs = c;
 
   return 0;
 }
 
 uint8_t serial_getchar(void) {
-  while( !(UCSR0A & (1<<RXC0)) );
-  return(UDR0);
+  while( !(UCSRsA & (1<<RXCs)) );
+  return(UDRs);
 }
--- serial.h
+++ serial.h
@@ -1,10 +1,60 @@
+// serial.h
+// author: Christophe VG <contact@christophe.vg>
+
+// functions to operate the UART on AVR/ATMega
+
+#ifndef __SERIAL_H
+#define __SERIAL_H
+
 // compute baud rate setting
-#define BAUD   9600
-#define MYUBRR (F_CPU/16/BAUD-1)
+#ifndef BAUD
+#define BAUD 9600
+#endif
+
+#include <util/setbaud.h>
 
 #include <stdio.h>
 #include <avr/io.h>
 
+#include "avr.h"
+
+// some AVR devices have multiple USARTs
+// in the actual identifiers are the idenfying 0 or 1 is replaced by an 's'
+
+#ifdef SERIAL_ON_USART1   // USART1 (e.g. on the ATMEGA1284p)
+#define UBRRsH UBRR1H
+#define UBRRsL UBRR1L  
+#define UCSRsA UCSR1A
+#define U2Xs   U2X1
+#define UCSRsC UCSR1C
+#define UCSRsB UCSR1B
+#define UCSZs1 UCSZ11
+#define UCSZs0 UCSZ10
+#define RXENs  RXEN1
+#define TXENs  TXEN1
+#define UDREs  UDRE1
+#define RXCs   RXC1
+#define UDRs   UDR1
+#else                     // USART0 (default)
+#define UBRRsH UBRR0H
+#define UBRRsL UBRR0L  
+#define UCSRsA UCSR0A
+#define U2Xs   U2X0
+#define UCSRsC UCSR0C
+#define UCSRsB UCSR0B
+#define UCSZs1 UCSZ01
+#define UCSZs0 UCSZ00
+#define RXENs  RXEN0
+#define TXENs  TXEN0
+#define UDREs  UDRE0
+#define RXCs   RXC0
+#define UDRs   UDR0
+#endif
+
+// public functions
+
 void       serial_init(void);
 static int serial_putchar(char c, FILE *stream);
 uint8_t    serial_getchar(void);
+
+#endif
