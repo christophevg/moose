a86168a8101a806071bc9f7f61391a6ec3544735
Christophe VG <xtof@astroboy.local>
2013-12-02 22:51:26 +0100

    added draft clock counting ms

---
+++ clock.c
@@ -0,0 +1,32 @@
+// clock.c
+// author: Christophe VG <contact@christophe.vg>
+
+// a clock that counts miliseconds
+
+// this code sets up timer1 for a 1ms  @ 8Mhz Clock (Mode 12)
+
+// TODO: make this ATMEGA1284p-only code useable on ATMEGA8 & co
+
+#include "clock.h"
+
+static unsigned long ticks = 0;
+
+void clock_init(void) { 
+  // Clock Speed (8000000) / PreScaler(64) = 125000
+  // * timeout (0.001sec)                  = 125
+  // - 1                                   = 124
+  ICR1 = 124;
+
+  TCCR1B |= (1 << WGM13) | (1 << WGM12); // mode 12, CTC on ICR1
+  TCCR1B |= (1 << CS10)  | (1 << CS11);  // prescaler of 64
+    
+  TIMSK1 |= (1 << OCIE1B);               // enable CTC interrupt 
+} 
+
+unsigned long clock_get_millis(void) {
+  return ticks;
+}
+
+ISR (TIMER1_COMPB_vect) {
+  ticks++;
+}
---
+++ clock.h
@@ -0,0 +1,17 @@
+// clock.h
+// author: Christophe VG <contact@christophe.vg>
+
+// a clock that counts miliseconds
+
+// this code sets up timer1 for a 1ms  @ 8Mhz Clock (Mode 12)
+
+#ifndef __CLOCK_H
+#define __CLOCK_H
+
+#include <avr/io.h>
+#include <avr/interrupt.h>
+
+void clock_init(void);
+unsigned long clock_get_millis(void);
+
+#endif
